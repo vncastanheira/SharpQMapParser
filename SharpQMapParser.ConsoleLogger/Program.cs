37ea839acaef96f09e47a31ef65d47f5c411538e
Epiplon <epiplon@epiplon.com>
2022-12-31 22:46:50 -0300

    Added timer counter

--- SharpQMapParser.ConsoleLogger/Program.cs
+++ SharpQMapParser.ConsoleLogger/Program.cs
@@ -1,5 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using SharpQMapParser;
+using System.Diagnostics;
 
 if (args.Length> 0)
 {
@@ -9,15 +10,21 @@ if (args.Length> 0)
 		var map = new Map();
 		try
 		{
-			using (StreamReader streamReader = File.OpenText(path))
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+            using (StreamReader streamReader = File.OpenText(path))
 			{
 				map.Parse(streamReader);
 			}
+			stopWatch.Stop();
 
-			Console.WriteLine("Map parsed sucessfully.");
+            Console.WriteLine("Map parsed sucessfully.");
 			Console.WriteLine($"Entities found: {map.Entities.Count}");
 			var worldspawn = map.Entities.Find(e => e.ClassName == "worldspawn");
             Console.WriteLine($"Brushes found: {worldspawn.Brushes.Count}");
+            TimeSpan ts = stopWatch.Elapsed;
+            string elapsedTime = string.Format("{0:00}:{1:00}:{2:00}.{3:00}", ts.Hours, ts.Minutes, ts.Seconds, ts.Milliseconds / 10);
+            Console.WriteLine("Parsing Runtime: " + elapsedTime);
 
         }
 		catch (Exception ex)
